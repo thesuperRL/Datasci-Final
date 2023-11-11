df9ad779d101b0211d850a6a748d62beca5b6826
thesuperRL <89622221+thesuperRL@users.noreply.github.com>
2023-11-10 19:56:01 -0800

    Update training.py

--- training.py
+++ training.py
@@ -11,13 +11,17 @@ from sklearn.model_selection import train_test_split
 import time
 
 class Model:
-    def __init__(self):
+    def __init__(self, train):
         model = Sequential()
 
-        model.add(Dense(10, activation='softmax'))
+        model.add(Dense(128, activation='softmax', input_dim = train.shape[1]))
+        model.add(Dense(256, activation='softmax'))
+        model.add(Dense(256, activation='softmax'))
         model.add(Dropout(0.5))
-        model.add(Dense(10, activation='softmax'))
+        model.add(Dense(256, activation='softmax'))
         model.add(Dropout(0.25))
         model.add(Dense(1, activation='softmax'))
+        
+        model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_squared_error'])
 
-        model.summary()
+        print(model.summary())
