bd84130465de299b41be671f18b84dc606b7cdc3
thesuperRL <89622221+thesuperRL@users.noreply.github.com>
2023-11-14 19:39:16 -0800

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -5,9 +5,9 @@ rawPoverty = pd.read_csv("Poverty.csv")
 rawUnemployment = pd.read_csv("unemployment.csv")
 rawArea = pd.read_csv("area.csv")
 
-print(rawPoverty)
-
 crimesPoverty = pd.merge(rawCrimes, rawPoverty,  how='left', left_on=['year','state_name'], right_on = ['year','state'])
-crimesPoverty.drop(columns = ["5"])
+crimesPoverty = crimesPoverty[crimesPoverty['state'].notna()] # cleans out all previous years in the crimes dataset
 
-print(crimesPoverty)
+cPU = pd.merge(crimesPoverty, rawUnemployment,  how='left', left_on=['year','state_name'], right_on = ['year','state'])
+
+print(cPU)
