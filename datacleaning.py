d3e4c287ba208af1a499f5d0c7977abcc5cac676
thesuperRL <89622221+thesuperRL@users.noreply.github.com>
2023-11-14 19:35:02 -0800

    fix pov, add datac

--- datacleaning.py
+++ datacleaning.py
@@ -1,2 +1,13 @@
-import pandas
+import pandas as pd
 
+rawCrimes = pd.read_csv("Crimes.csv")
+rawPoverty = pd.read_csv("Poverty.csv")
+rawUnemployment = pd.read_csv("unemployment.csv")
+rawArea = pd.read_csv("area.csv")
+
+print(rawPoverty)
+
+crimesPoverty = pd.merge(rawCrimes, rawPoverty,  how='left', left_on=['year','state_name'], right_on = ['year','state'])
+crimesPoverty.drop(columns = ["5"])
+
+print(crimesPoverty)
